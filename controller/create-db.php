2faf3ed1196e6b2bbe0c41d90dd7647523cbb57d
Edgar Cantero <cantero098@gmail.com>
2015-02-10 09:22:00 -0800

    seee im commiting

--- controller/create-db.php
+++ controller/create-db.php
@@ -19,6 +19,9 @@ else {
 }
 
 $query = $connection->query("CREATE TABLE posts ("
-        ."id int(11) NOT NULL");
+        . "id int(11) NOT NULL AUTO_INCREMENT,"
+        . "title varchar(255) NOT NULL,"
+        . "post text NOT NULL,"
+        . "PRIMARY KEY (id))");
 
 $connection->close();
