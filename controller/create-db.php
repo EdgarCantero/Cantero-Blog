140db4e7e199a856d3e6d01324a214d28a8e0609
Edgar Cantero <cantero098@gmail.com>
2015-02-18 09:20:34 -0800

    vid 37

--- controller/create-db.php
+++ controller/create-db.php
@@ -1,27 +1,27 @@
 <?php
 require_once(__DIR__ . "/../model/config.php");
 
-$connection = new mysqli($host, $username, $password);
-if($connection->connect_error){
-    die ("Error: ". $connection->connect_error);
-}
-$exists = $connection->select_db($database);
-
-if(!$exists){
-    $query = $connection ->query ("CREATE DATABASE $database ");
-    
-    if($query){
-        echo "successfully created database " . $database;
-    }
-}
-else {
-    echo"Database already exists.";
-}
-
-$query = $connection->query("CREATE TABLE posts ("
+$query = $_SESSION["connection"]->query("CREATE TABLE posts ("
         . "id int(11) NOT NULL AUTO_INCREMENT,"
         . "title varchar(255) NOT NULL,"
         . "post text NOT NULL,"
         . "PRIMARY KEY (id))");
 
-$connection->close();
+if($query) {
+    echo "<p>Succesfully created table: posts</p>";
+}
+else {
+    echo "<p>" . $_SESSION["connection"]->error . "</p>";
+}
+
+$query = $_SESSION["connection"]->query("CREATE TABLE users ("
+         . "id int(11)NOT NULL AUTO_INCREMENT,"
+         . "username varchar (30) NOT NULL,"
+         . "email varchar (50) NOT NULL,"
+         . "password char (128) NOT NULL,"
+         . "salt char(128) NOT NULL,"
+         . "PRIMARY KEY (id)");
+
+if($query) {
+     echo ""
+}
--- model/Database.php
+++ model/Database.php
@@ -5,6 +5,7 @@ class Database {
     private $username;
     private $password;
     private $database;
+    public  $error;
     
     public function __construct($host, $username, $password, $database){
     $this->host = $host; 
@@ -12,12 +13,32 @@ class Database {
     $this->password = $password;
     $this->database = $database;
     
+    $connection = new mysqli($host, $username, $password);
+    
+    if ($connection->connect_error) {
+        die("<p>Error: " . $connection->connect_error . "</p>");
+    }
+    
+    $exists = $connection->select_db($database);
+    
+    if (!$exists) {
+    $query = $connection->query("CREATE DATABASE $database");
+    
+    if ($query) {
+        echo"<P>Successfully created database: " . $database ." </P>";
+      }
+    }else {
+        echo "<p>Database already exists. </p>";
+    }
+    
     }
     
     public function openConnection() {
-        $this->connection = new mysqli($this->host, $this->username, $this->password, $this->password, $this->database);
-    if ($connection->connect_error) {
-        die("<p>Error: " . $connection->connect_error . "</p>");
+        $this->connection = new mysqli($this->host, $this->username, $this->password, $this->database);
+    
+        
+    if ($this->connection->connect_error) {
+        die("<p>Error: " . $this->connection->connect_error . "</p>");
     }
                 }
     
@@ -31,6 +52,14 @@ class Database {
         $this->openConnection();
         
         $query = $this->connection->query($string);
+        
+        if(!$query) {
+            $this->error = $this->connection->error;
+        }
+        
+        $this->closeConnection();
+        
+        return $query;
     }
 }
 
