89db3ce9e49145b81fff087dc016f819b43eb129
Edgar Cantero <cantero098@gmail.com>
2015-02-20 09:21:04 -0800

    done vid 41

--- view/register-form.php
+++ view/register-form.php
@@ -1,7 +1,10 @@
 <?php
    require_once(__DIR__ . "/../model/config.php");
 ?>
-<form>
+
+<h1>Register</h1>
+
+<form method="post" action="<?php echo $path . "controller/create-user.php"; ?>">
     <div>
         <label for="email">Email: </label>
         <input type="text" name="email" />
