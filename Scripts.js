68f4b3fabbda4450c3653c4029c67aaef7f2198a
Edgar Cantero <cantero098@gmail.com>
2015-02-26 09:21:11 -0800

    Nooooooooooooooooooooooooooooooooo   NEEEDDDSSSS MORE TIMMMEEEEEEEEEEEESSSSSSSSSSS

---
+++ Scripts.js
@@ -0,0 +1,11 @@
+$("document") .ready (function(){
+    $(".class").css("color", "black").css("font-style","italic").css("background-color", "gray") ;
+    $('p:last').css("color", "black").css("font-style","");
+    
+    $('p:gt(4)').css('border-style', 'solid');
+    
+     
+     $('body').css('text-align', "center");
+  
+
+});
--- view/header.php
+++ view/header.php
@@ -1,12 +1,23 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
+
 <html>
     <head>
-        <meta charset="UTF-8">
-        <title></title>
+        <title>Cantero blog</title>
+          <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link type="text/css" rel="stylesheet" href="css/bootstrap.css">
+        <link type="text/css" rel="stylesheet" href="bootstrap-theme.css">
+        
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script> 
+        <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/themes/smoothness/jquery-ui.css" />
+        <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js"></script>
     </head>
-    <body>
+    <body id="body">
+        <header>
+            <nav>
+            <a href="login.php">Login</a>
+            <a href="post.php">Create Post</a>
+            <a href="index.php">Blog</a>
+            <a href="http://localhost/Cantero-Blog/view/register-form.php">Register</a>
+            </nav>
+        </header>
+
